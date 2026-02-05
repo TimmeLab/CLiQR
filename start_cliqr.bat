daae1556d2ef74429bd5bc540e78af0274a17a34
cparker-uc <parkecp@mail.uc.edu>
2026-02-05 13:50:12 -0500

    Updated Windows batch script for starting the GUI

--- start_cliqr.bat
+++ start_cliqr.bat
@@ -1,5 +1,30 @@
-set "MF=%USERPROFILE%\AppData\Local\miniforge3"
-call "%MF%\Scripts\activate.bat" "%MF%"
+@echo off
+REM CLiQR Recording System Launcher
+REM This script starts the Solara GUI for data recording
+
+echo ==========================================
+echo    CLiQR Recording System
+echo    Timme Lab - University of Cincinnati
+echo ==========================================
+echo.
+
+REM Activate the conda environment
+echo Activating environment...
 call conda activate cliqr
-cd /d "%SUERPROFILE%\cliqr"
-jupyter-lab
+if errorlevel 1 (
+    echo ERROR: Could not activate 'cliqr' environment
+    echo Please run: conda env create --file environment.yml
+    pause
+    exit /b 1
+)
+
+echo Starting application...
+echo.
+echo The application will open in your default browser.
+echo When finished, close the browser and press Ctrl+C here.
+echo.
+
+REM Start the Solara app
+solara run recording_gui.py
+
+pause
